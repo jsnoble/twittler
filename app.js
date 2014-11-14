fa48d21eaaf59b02a14aa486dfdf09679803d0e1
Jared Noble <jsnoble1@gmail.com>
2014-11-14 13:13:01 -0700

    added link username

--- app.js
+++ app.js
@@ -1,24 +1,37 @@
- $(document).ready(function() {
+$(document).ready(function() {
 
 
-     var $article = $('article');
+    var $article = $('article');
 
 
-     function addtwit(item) {
+    function addtwit(item) {
+        var $tweet = $('<div class="indtweet"></div>');
+        var $user = $('<a href= "#" class="name"></a>');
+        var $message = $('<span class="message"></span>');
+        var $time = $('<span class="time"></span>');
+        $user.text('@' + item.user);
+        $message.text(" " + item.message);
+        $time.text(" time: " + item.created_at);
 
-         var $tweet = $('<div></div>');
-         $tweet.text('@' + item.user + ': ' + item.message +"   time: " + item.created_at);
-         $tweet.appendTo($article);
-         ind += 1
-     }
-     var ind = 0;
-     streams.home.forEach(addtwit);
+        $tweet.append($user, $message, $time);
+        $tweet.appendTo($article)
+
+        ind += 1
+    }
+    var ind = 0;
+    streams.home.forEach(addtwit);
+
+    $(document).on("click", ".name", function() {
+        var user = $(this).text().slice(1);
+        $('.indtweet').remove();
+        streams.users[user].forEach(addtwit);
+    });
 
 
-     $('button').on('click', function() {
-         var ix = ind;
-         var arr = streams.home.slice(ix, streams.home.length);
-         arr.forEach(addtwit);
-     });
+    $('button').on('click', function() {
+        var ix = ind;
+        var arr = streams.home.slice(ix, streams.home.length);
+        arr.forEach(addtwit);
+    });
 
- });
+});
