bb4e464b07f00b242d397ccebba3c5ca07144259
Jared Noble <jsnoble1@gmail.com>
2014-11-14 08:21:10 -0700

    Moved the js out of html

---
+++ app.js
@@ -0,0 +1,24 @@
+ $(document).ready(function() {
+
+
+     var $article = $('article');
+
+
+     function addtwit(item) {
+
+         var $tweet = $('<div></div>');
+         $tweet.text('@' + item.user + ': ' + item.message);
+         $tweet.appendTo($article);
+         ind += 1
+     }
+     var ind = 0;
+     streams.home.forEach(addtwit);
+
+
+     $('button').on('click', function() {
+         var ix = ind;
+         var arr = streams.home.slice(ix, streams.home.length);
+         arr.forEach(addtwit);
+     });
+
+ });
